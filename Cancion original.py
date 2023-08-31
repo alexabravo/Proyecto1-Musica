1d49df3af5acf1acd5deac3d9d60b1e8d3d8b68d
coutino568 <xplay568@gmail.com>
2023-08-31 00:20:41 -0600

    backgorund instrument

--- Cancion original.py
+++ Cancion original.py
@@ -6,9 +6,10 @@ Jupyter = Score("Jupyter", 108.0)  # tempo is 108 bpm
 
 #Candidatos: 97 ; 83 
 MelodyPart    = Part(83, 0)        # Melody  part on channel 0
+BackgroundPart = Part(62,1)
 
 themePhrase = Phrase(0.0)            # theme starts at the beginning
- 
+backgroundPhrase= Phrase(0.0)
 
 
 #pitches1 = [G2,G2,G2,B2,B2,G2,G2,D2]
@@ -20,27 +21,35 @@ pitches2 = [G2,G2,G2,C2,D2,G2,G2,D2]
 durations2= [DEN, DEN,DEN, QN,QN, DEN,DEN, HN]
 
 
+pitches3 = [G2,G2,G2,C2,D2,G2,G2,D2]
+durations3= [DEN, DEN,QN, DEN,QN, DEN,HN, DEN]
 # add the notes to the theme
 themePhrase.addNoteList(pitches1, durations1)
 themePhrase.addNoteList(pitches2, durations2)
 
+backgroundPhrase.addNoteList(pitches3, durations2)
+
 # make two new phrases and change start times to make a round
 response1Phrase = themePhrase.copy()
-
-response1Phrase.setStartTime(8.0)     # start after 4 quarter notes
+# cada set dura 13/8 QN 
+backgroundPhrase.setStartTime(23.0)     # start after 4 quarter notes
 
 # play different parts in different registers
 Mod.transpose(themePhrase, 12)         # one octave higher
 
 # play each phrase twice
 Mod.repeat(themePhrase, 5)
-Mod.repeat(response1Phrase, 5)
+Mod.repeat(backgroundPhrase, 5)
 
 # add phrases to corresponding parts
 MelodyPart.addPhrase(themePhrase)
+BackgroundPart.addPhrase(backgroundPhrase)
+
 
 # add parts to score
 Jupyter.addPart(MelodyPart)
+Jupyter.addPart(BackgroundPart)
+
 
 # play score
 Play.midi(Jupyter)
